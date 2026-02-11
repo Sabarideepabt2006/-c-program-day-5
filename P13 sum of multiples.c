501d06d7b7a189ab11dbcc3498bbbba44a9315ab
SABARIDEEPA.B.T <sabarideepa.bt2006@gmail.com>
2026-02-11 15:42:53 +0530

    Add files via upload

---
+++ P13 sum of multiples.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n,m;
+    int sum=0;
+    scanf("%d%d",&n,&m);
+    for (int i=1;i<=m;i++) {
+        if (i%n==0) {
+            sum=sum+i;
+        }
+    }
+    printf("%d\n",sum);
+    return 0;
+}
