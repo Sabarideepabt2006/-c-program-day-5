0a9fd6360fa78f0d468d6a2cd25185a565610a6b
SABARIDEEPA.B.T <sabarideepa.bt2006@gmail.com>
2026-02-12 21:42:26 +0530

    Add files via upload

---
+++ P9 nested for loop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+  for (int i=1;i<=5;i++) {
+    for (int j=1;j<=5-i;j++) {
+      printf(" ");
+    }
+    for (int j=1;j<=2*i-1;j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+    for (int i=5-1;i>=1;i--) {
+      for (int j=5;j>i;j--) {
+        printf(" ");
+      }
+      for (int k=1;k<=2*i-1;k++) {
+        printf("*");
+      }
+      printf("\n");
+  }
+ return 0;
+}
